76945e00b31262361d02826afab70cfc612cd624
liyuntao <liyuntao58607@gmail.com>
2021-12-03 16:43:18 +0800

    init

---
+++ examples/day01.rs
@@ -0,0 +1,26 @@
+use std::fs::File;
+use std::io::{BufRead, BufReader, Error, ErrorKind};
+
+pub fn read_input(file_name: &str) -> Result<Vec<i32>, Error> {
+    let path = format!("./input/{}", file_name);
+    let br = BufReader::new(File::open(path)?);
+    br.lines()
+        .map(|line| line.and_then(|v| v.parse().map_err(|e| Error::new(ErrorKind::InvalidData, e))))
+        .collect()
+}
+
+fn count_total_incr(inputs: &[i32]) -> usize {
+    let len = inputs.len();
+    inputs[0..len - 1].iter().zip(&inputs[1..len])
+        .filter(|tuple| tuple.1 > tuple.0)
+        .count()
+}
+
+fn main() {
+    let numbers = read_input("day01.txt").unwrap();
+    // q1: count total increases window by 1
+    println!("q1={}", count_total_incr(&numbers));
+    // q2: count total increases window by 3
+    let win_by_3then_sum: Vec<i32> = numbers.windows(3).map(|win| win.iter().sum()).collect();
+    println!("q2={}", count_total_incr(&win_by_3then_sum));
+}
---
+++ readme.md
@@ -0,0 +1,25 @@
+## Examples of how to use adventofcode2021
+
+This directory contains a number of examples showcasing the solutions of [Advent Of Code 2021](https://adventofcode.com/2021).
+
+All solutions can be executed with:
+
+```
+cargo run --example $name
+
+# e.g. 
+cargo run --example day01
+```
+
+
+
+```
+# you may have to put your input into ./input dir
+
+# if you wanna make it run faster:
+cargo run --example day01 --release
+
+# measure execution time
+time cargo run --example day01 --release
+```
+
