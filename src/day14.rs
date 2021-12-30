816a13850ef009fa1508fa13af452261167b8660
liyuntao <liyuntao58607@gmail.com>
2021-12-30 23:25:30 +0800

    day13 + day14 q1

--- src/day14.rs
+++ src/day14.rs
@@ -1,29 +1,50 @@
 use std::collections::HashMap;
 
-const INPUT: &'static str = include_str!("../input/day13.txt");
-
+const INPUT: &'static str = include_str!("../input/day14.txt");
 
 fn parse_input(input: &str) -> String {
     input.lines().collect::<Vec<&str>>()[0].to_string()
 }
 
-fn parse_map(input: &str) -> HashMap<(char, char), char> {
-    input.lines()
+fn parse_dict(input: &str) -> HashMap<(char, char), char> {
+    input
+        .lines()
         .filter(|line| line.contains('-'))
         .map(|line| {
             let chars: Vec<char> = line.chars().collect();
             ((chars[0], chars[1]), chars[6])
-        }).collect()
+        })
+        .collect()
 }
 
-fn
+fn tick(chars: Vec<char>, dict: &HashMap<(char, char), char>) -> Vec<char> {
+    let last_char = *chars.last().unwrap();
+    // array concat
+    let mut concat_arr = chars
+        .windows(2)
+        .map(|arr| [arr[0], *dict.get(&(arr[0], arr[1])).unwrap()])
+        .fold(vec![], |mut acc, inc| {
+            acc.extend_from_slice(&inc);
+            acc
+        });
+    concat_arr.push(last_char);
+    concat_arr
+}
 
 fn q1(input: &str) -> usize {
     let line = parse_input(input);
+    let dict = parse_dict(input);
     let input_chars: Vec<char> = line.chars().collect();
-    // input_chars.windows(2)
-    println!("{}", line);
-    1588
+
+    let result = (0..10).fold(input_chars, |acc, _i| tick(acc, &dict));
+    let cnt_map = result.iter().fold(HashMap::new(), |mut acc, c| {
+        *acc.entry(c).or_insert(0) += 1;
+        acc
+    });
+
+    let max_cnt = cnt_map.iter().max_by(|&a, &b| a.1.cmp(b.1)).unwrap().1;
+    let min_cnt = cnt_map.iter().min_by(|&a, &b| a.1.cmp(b.1)).unwrap().1;
+    max_cnt - min_cnt
 }
 
 pub(crate) fn run() {
